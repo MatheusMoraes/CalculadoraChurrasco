a06265134fbb326fc0d1e3c857bf147d9b168e68
matheussilva201654@gmail.com <matheussilva201654@gmail.com>
2019-10-01 15:18:35 -0300

    Inicialização

---
+++ VemProChurras/VemProChurras/VemProChurras/InitialPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VemProChurras.Views;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace VemProChurras
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class InitialPage : ContentPage
+	{
+		public InitialPage ()
+		{
+			InitializeComponent ();
+		}
+
+        private async void ImageButton_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new PaginaBoi());
+        }
+
+
+        private async void Pagina_Galinha(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new PaginaGalinha());
+
+        }
+
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new MainPage());
+        }
+
+        private async void Pagina_Porco(object sender,EventArgs e)
+        {
+            await Navigation.PushAsync(new PaginaPorco());
+        }
+    }
+}
---
+++ VemProChurras/VemProChurras/VemProChurras/LoadingPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VemProChurras.Views;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace VemProChurras
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class LoadingPage : ContentPage
+	{
+		public LoadingPage ()
+		{
+			InitializeComponent ();
+		}
+
+
+        private async void ImageButton_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new PaginaBoi());
+        }
+    }
+}
