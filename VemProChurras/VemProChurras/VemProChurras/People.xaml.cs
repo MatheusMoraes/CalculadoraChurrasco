a97b52a91622e7a435d05aab25858ce3d03621dd
Matheus Rocha <mathrochaa@hotmail.com>
2019-10-02 22:38:42 -0300

    Criei a content page people

---
+++ VemProChurras/VemProChurras/VemProChurras/People.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace VemProChurras
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class People : ContentPage
+	{
+		public People ()
+		{
+			InitializeComponent ();
+		}
+        private void hSlider_ValueChanged (object sender, ValueChangedEventArgs e)
+        {
+            hTxtResult.Text = "Homens:" + Convert.ToInt16(hSlider.Value);
+        }
+        private void mSlider_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            mTxtResult.Text = "Mulheres:" + Convert.ToInt16(mSlider.Value);
+        }
+        private void cSlider_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            cTxtResult.Text = "Crianças:" + Convert.ToInt16(cSlider.Value);
+        }
+
+    }
+}
