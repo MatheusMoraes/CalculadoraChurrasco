85a5cea548b221f34463638aa5cfbf0cc79a3fdc
matheussilva201654@gmail.com <matheussilva201654@gmail.com>
2019-10-05 10:32:22 -0300

    Novas Páginas Descartáveis, Bebidas e Acompanhamentos

---
+++ VemProChurras/VemProChurras/VemProChurras/PaginaBebidas.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace VemProChurras
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class PaginaBebidas : ContentPage
+	{
+		public PaginaBebidas ()
+		{
+			InitializeComponent ();
+		}
+
+        private async void GoPaginaAcompanhamentos_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new PaginaAcompanhamentos());
+
+        }
+    }
+}
