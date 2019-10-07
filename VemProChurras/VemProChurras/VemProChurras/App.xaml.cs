2f7dc20fc79af45e55683547433f0005f39dcf16
Matheus Rocha <mathrochaa@hotmail.com>
2019-10-07 16:03:53 -0300

    .

--- VemProChurras/VemProChurras/VemProChurras/App.xaml.cs
+++ VemProChurras/VemProChurras/VemProChurras/App.xaml.cs
@@ -11,8 +11,7 @@ namespace VemProChurras
         {
             InitializeComponent();
 
-            MainPage = new People(); 
-                //new NavigationPage(new PaginaBoi()) { BarBackgroundColor = Color.FromRgb(245, 123, 74) };
+            MainPage = new NavigationPage(new PaginaBoi()) { BarBackgroundColor = Color.FromRgb(245, 123, 74) };
         }
 
         protected override void OnStart()
