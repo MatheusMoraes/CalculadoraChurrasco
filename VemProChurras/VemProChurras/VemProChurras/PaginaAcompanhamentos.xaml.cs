aa84b18678bf922421365c18436a918363388556
Matheus Rocha <mathrochaa@hotmail.com>
2019-10-10 22:42:23 -0300

    to fazendo

--- VemProChurras/VemProChurras/VemProChurras/PaginaAcompanhamentos.xaml.cs
+++ VemProChurras/VemProChurras/VemProChurras/PaginaAcompanhamentos.xaml.cs
@@ -19,7 +19,43 @@ namespace VemProChurras
 
         private async void GoPaginaDescartaveis_Clicked(object sender, EventArgs e)
         {
-            await Navigation.PushAsync(new PaginaDescartaveis());
+            if (SwitchCerveja.IsToggled == true) ValorCerveja1 = 30;
+            else ValorCerveja1 = 0;
+
+            if (SwitchCaipirinha.IsToggled == true) ValorCaipirinha1 = 30;
+            else ValorCaipirinha1 = 0;
+
+            if (SwitchBeats.IsToggled == true) ValorBeats1 = 30;
+            else ValorBeats1 = 0;
+            if (SwitchSuco.IsToggled == true) ValorSuco1 = 30;
+            else ValorSuco1 = 0;
+
+            if (SwitchVodka.IsToggled == true) ValorVodka1 = 30;
+            else ValorVodka1 = 0;
+
+            if (SwitchRefrigerante.IsToggled == true) ValorRefrigerante1 = 30;
+            else ValorRefrigerante1 = 0;
+
+            Variaveis variaveis = new Variaveis
+            {
+                NumHomem = Convert.ToInt32(lblHomem.Text),
+                NumMulher = Convert.ToInt32(lblMulher.Text),
+                NumCrianca = Convert.ToInt32(lblCrianca.Text),
+                ValorCarneAves = Convert.ToInt32(lblAves.Text),
+                ValorCarneBovina = Convert.ToInt32(lblBovina.Text),
+                ValorCarneSuina = Convert.ToInt32(lblSuina.Text),
+                ValorCerveja = Convert.ToInt32(lblSuina.Text),
+                ValorCaipirinha = Convert.ToInt32(lblSuina.Text),
+                ValorBeats = Convert.ToInt32(lblSuina.Text),
+                ValorSuco = Convert.ToInt32(lblSuina.Text),
+                ValorVodka = Convert.ToInt32(lblSuina.Text),
+                ValorRefrigerante = Convert.ToInt32(lblSuina.Text),
+
+            };
+            var paginaDescartaveis = new PaginaDescartaveis();
+            paginaDescartaveis.BindingContext = variaveis;
+            await Navigation.PushAsync(paginaDescartaveis);
+
 
         }
     }
--- VemProChurras/VemProChurras/VemProChurras/Variaveis.cs
+++ VemProChurras/VemProChurras/VemProChurras/Variaveis.cs
@@ -6,18 +6,28 @@ namespace VemProChurras
 {
     public class Variaveis
     {
+        //pessoas
         public int NumHomem  { get; set; }
         public int NumMulher  { get; set; }
         public int NumCrianca { get; set; }
+        //carnes
         public int ValorCarneAves { get; set; }
         public int ValorCarneBovina { get; set; }
         public int ValorCarneSuina { get; set; }
+        //bebidas
         public int ValorCerveja { get;  set; }
         public int ValorCaipirinha { get;  set; }
         public int ValorBeats { get; set; }
         public int ValorSuco { get; set; }
         public int ValorVodka { get; set; }
         public int ValorRefrigerante { get; set; }
+        //acompanhamentos
+        public int ValorArrozAGrega { get; set; }
+        public int ValorQueijoCoalho { get; set; }
+        public int ValorFarofa { get; set; }
+        public int ValorVinagrete { get; set; }
+        public int ValorSaladaMaio { get; set; }
+        public int ValorPao { get; set; }
 
     }
 
