730e8dea17e5e4e21665f00aaad69b60d465efad
Matheus Rocha <mathrochaa@hotmail.com>
2019-10-10 22:01:54 -0300

    foda

--- VemProChurras/VemProChurras/VemProChurras/PaginaDeCarnes.xaml.cs
+++ VemProChurras/VemProChurras/VemProChurras/PaginaDeCarnes.xaml.cs
@@ -9,27 +9,42 @@ using Xamarin.Forms.Xaml;
 
 namespace VemProChurras
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class PaginaDeCarnes : ContentPage
-	{
-		public PaginaDeCarnes ()
-		{
-			InitializeComponent ();
-            
-		}
-
-
-        private async void PaginaBebida(object sender, EventArgs e)
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PaginaDeCarnes : ContentPage
+    {
+        public PaginaDeCarnes()
         {
-            var variaveis = new Variaveis
-            {
-                CarneAves = SwitchAves.IsToggled.Equals(30)
-            };
-            var pagBebidas = new PaginaBebidas();
-            pagBebidas.BindingContext = variaveis;
-            await Navigation.PushAsync(pagBebidas);
+            InitializeComponent();
+
         }
 
+        public int ValorCarneBovina1 { get; private set; }
+        public int ValorCarneAves1 { get; private set; }
+        public int ValorCarneSuino1 { get; private set; }
 
-	}
+        private async void PaginaBebidas(object sender, EventArgs e)
+        {
+            if (SwitchBovina.IsToggled == true) ValorCarneBovina1 = 30;
+            else ValorCarneBovina1 = 0;
+
+            if (SwitchAves.IsToggled == true) ValorCarneAves1 = 30;
+            else ValorCarneAves1 = 0;
+
+            if (SwitchSuino.IsToggled == true) ValorCarneSuino1 = 30;
+            else ValorCarneSuino1 = 0;
+
+            Variaveis variaveis = new Variaveis
+            {
+                NumHomem = Convert.ToInt32(lblHomem.Text),
+                NumMulher = Convert.ToInt32(lblMulher.Text),
+                NumCrianca = Convert.ToInt32(lblCrianca.Text),
+                ValorCarneAves = ValorCarneAves1,
+                ValorCarneBovina = ValorCarneBovina1,
+                ValorCarneSuina = ValorCarneSuino1
+            };
+            var paginaBebidas = new PaginaBebidas();
+            paginaBebidas.BindingContext = variaveis;
+            await Navigation.PushAsync(paginaBebidas);
+        }
+    }
 }
--- VemProChurras/VemProChurras/VemProChurras/Variaveis.cs
+++ VemProChurras/VemProChurras/VemProChurras/Variaveis.cs
@@ -4,14 +4,15 @@ using System.Text;
 
 namespace VemProChurras
 {
-    class Variaveis
+    public class Variaveis
     {
         public int NumHomem  { get; set; }
         public int NumMulher  { get; set; }
         public int NumCrianca { get; set; }
-        public bool CarneAves { get; set; }
-        public bool CarneBovina { get; set; }
-        public bool CarneSuina { get; set; }
+        public int ValorCarneAves { get; set; }
+        public int ValorCarneBovina { get; set; }
+        public int ValorCarneSuina { get; set; }
+
 
     }
 
