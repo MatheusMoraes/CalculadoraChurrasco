386ca8bfd39bcee29f7169122dee0d6f23549c93
matheussilva201654@gmail.com <matheussilva201654@gmail.com>
2019-10-13 12:42:31 -0300

    123

--- VemProChurras/VemProChurras/VemProChurras/People.xaml.cs
+++ VemProChurras/VemProChurras/VemProChurras/People.xaml.cs
@@ -15,23 +15,37 @@ namespace VemProChurras
 		public People ()
 		{
 			InitializeComponent ();
+
+
+          
 		}
-        private void hSlider_ValueChanged (object sender, ValueChangedEventArgs e)
-        {
-            hTxtResult.Text = "Homens:" + Convert.ToInt16(hSlider.Value);
-        }
-        private void mSlider_ValueChanged(object sender, ValueChangedEventArgs e)
-        {
-            mTxtResult.Text = "Mulheres:" + Convert.ToInt16(mSlider.Value);
-        }
-        private void cSlider_ValueChanged(object sender, ValueChangedEventArgs e)
-        {
-            cTxtResult.Text = "Crianças:" + Convert.ToInt16(cSlider.Value);
-        }
+        //private void hSlider_ValueChanged (object sender, ValueChangedEventArgs e)
+        //{
+        //    hTxtResult.Text = "Homens:" + Convert.ToInt16(hSlider.Value);
+          
+        //}
+        //private void mSlider_ValueChanged(object sender, ValueChangedEventArgs e)
+        //{
+        //    mTxtResult.Text = "Mulheres:" + Convert.ToInt16(mSlider.Value);
+        //}
+        //private void cSlider_ValueChanged(object sender, ValueChangedEventArgs e)
+        //{
+        //    cTxtResult.Text = "Crianças:" + Convert.ToInt16(cSlider.Value);
+        //}
 
         private async void PaginaCarnes(object sender, EventArgs e)
         {
-            await Navigation.PushAsync(new PaginaDeCarnes());
+
+            var churrasco = new Churrasco_Controller()
+            {
+                NumCrianca = Convert.ToInt32(Eteste.Text)
+               
+            };
+
+            var resultado = new Resultado();
+            resultado.BindingContext = churrasco;
+
+            await Navigation.PushAsync(new Resultado());
 
         }
 
